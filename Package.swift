816a8a54657e451522039ea56413f3e960dc41b0
Todd Cowing <toddcowing@mac.com>
2026-04-28 13:34:05 -0700

    Package.swift: raise iOS floor v15 -> v17
    Cross-compile scan found this package uses iOS 17+ APIs in its own
    code (Observable / focusable / scrollPosition / KeyPress / onChange-initial /
    ContentUnavailableView). The v15 declaration did not survive iOS xcodebuild.
    Restoring manifest reality.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "TrackChangesPrimitive",
     platforms: [
         .macOS(.v14),
-        .iOS(.v15),
+        .iOS(.v17),
     ],
     products: [
         .library(name: "TrackChangesPrimitive", targets: ["TrackChangesPrimitive"]),
